dbf1a1f49c3eefad14cb0ffa99457ba22098b964
henrylong <henrylong719@gmail.com>
2025-03-16 22:45:31 +1100

    add first neuron note

--- 10 - First Neuron/neuron.py
+++ 10 - First Neuron/neuron.py
@@ -8,17 +8,21 @@ X = torch.tensor([
     [150.0]
 ])
 
-model = nn.Linear(1, 1)
+# 1 feature and 1 output
+model = nn.Linear(1,1)
 
 model.bias = nn.Parameter(
     torch.tensor([32.0])
 )
+
 model.weight = nn.Parameter(
     torch.tensor([[1.8]])
 )
 
 print(model.bias)
+
 print(model.weight)
 
 y_pred = model(X)
+
 print(y_pred)
