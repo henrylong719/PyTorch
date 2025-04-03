23f54c87bc0168da1056241f2058874f3014126b
henrylong <henrylong719@gmail.com>
2025-04-03 20:45:32 +1100

    add gitignore

--- 14 - Training Neuron/training_neuron.py
+++ 14 - Training Neuron/training_neuron.py
@@ -3,29 +3,35 @@ from torch import nn
 
 # Input: Temperature in °C
 X1 = torch.tensor([[10]], dtype=torch.float32) 
+
 # Actual value: Temperature °F
-y1 = torch.tensor([[50]], dtype=torch.float32) 
+y1 = torch.tensor([[50]], dtype=torch.float32)
 
 # Input: Temperature in °C
 X2 = torch.tensor([[37.78]], dtype=torch.float32) 
+
 # Actual value: Temperature °F
-y2 = torch.tensor([[100.0]], dtype=torch.float32) 
+y2 = torch.tensor([[100.0]], dtype=torch.float32)
 
 model = nn.Linear(1, 1)
+
+# Loss function (MSE): 𝐿 = (^𝑦 − y)^2
 loss_fn = torch.nn.MSELoss()
 optimizer = torch.optim.SGD(model.parameters(), lr=0.001)
 
-print(model.bias)
+print(model.weight)
 
 # Training pass
 optimizer.zero_grad()
 outputs = model(X1)
 loss = loss_fn(outputs, y1)
+
+# calcute gradients
 loss.backward()
 optimizer.step()
 
-print(model.bias)
-
+print(model.weight)
 
 y1_pred = model(X1)
-print("y1_pred =", y1_pred)
+print("y1_pred = ", y1_pred)
+
